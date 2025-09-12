8c56b41d7e09ce00781a5561ce34a6e34681cd07
gerry <krittameth2020@gmail.com>
2025-09-12 12:39:43 +0700

    Updated MainViewController.java Initialized all of the comboboxes

--- src/main/java/se233/audioconverterproject/controller/MainViewController.java
+++ src/main/java/se233/audioconverterproject/controller/MainViewController.java
@@ -18,7 +18,7 @@ public class MainViewController {
 
     @FXML private ComboBox<String> audioFormatComboBox;
 
-    @FXML private ComboBox<Integer> audioQualityComboBox;
+    @FXML private ComboBox<String> audioQualityComboBox;
 
     @FXML private ProgressBar progressBar;
     
@@ -26,7 +26,7 @@ public class MainViewController {
 
     @FXML private RadioButton monoRadio;
     @FXML private RadioButton stereoRadio;
-    @FXML private ComboBox<Integer> bitrateComboBox;
+    @FXML private ComboBox<String> bitrateComboBox;
 
     @FXML private RadioButton sampleRate41kRadio;
     @FXML private RadioButton sampleRate48kRadio;
@@ -36,12 +36,30 @@ public class MainViewController {
     private ToggleGroup qualityGroup;
 
     public static String[] formats = {"mp3", "wav", "ogg", "m4a", "flac"};
-    public static HashMap<String, Integer> qualities = (HashMap<String, Integer>) Map.ofEntries(
+    public static Map<String, Integer> qualities = new LinkedHashMap<>(Map.ofEntries(
             entry("Economy (64 kbps)", 64_000),
-            entry("Standard (128 kbps)", 128_000),
+            entry( "Standard (128 kbps)", 128_000),
             entry("Good (192 kbps)", 192_000),
-            entry("Best (320 kbps)", 320_000)
-    );
+            entry("Best (320 kbps)",320_000)
+    ));
+
+    public static Map<String, Integer> bitrates = new LinkedHashMap<>(Map.ofEntries(
+            entry("32 kbps", 32_000),
+            entry("40 kbps", 40_000),
+            entry("48 kbps", 48_000),
+            entry("56 kbps", 56_000),
+            entry("64 kbps", 64_000),
+            entry("80 kbps", 80_000),
+            entry("96 kbps",  96_000),
+            entry("112 kbps", 112_000),
+            entry("128 kbps", 128_000),
+            entry("160 kbps", 160_000),
+            entry("192 kbps",  192_000),
+            entry("224 kbps",  224_000),
+            entry("256 kbps", 256_000),
+            entry("320 kbps", 320_000)
+    ));
+
 
     public void initialize(){
         uploadIcon.setImage(new Image(Launcher.class.getResourceAsStream("music-file.png")));
@@ -55,5 +73,24 @@ public class MainViewController {
         this.sampleRate96kRadio.setToggleGroup(qualityGroup);
 
         audioFormatComboBox.getItems().addAll(formats);
+        audioFormatComboBox.getSelectionModel().select(0);  // SET DEFAULT VALUE TO `.mp3` FORMAT AND DISPLAY INSIDE THE FORMAT COMBOBOX
+
+
+        // CREATE ARRAY TO STORE QUALITY KEYS AND DISPLAY INSIDE THE QUALITY COMBOBOX
+        List<Map.Entry<String, Integer>> sortedList = new ArrayList<>(qualities.entrySet());
+        sortedList.sort(Map.Entry.comparingByValue());
+        for (Map.Entry<String, Integer> entry : sortedList) {
+            audioQualityComboBox.getItems().add(entry.getKey());
+        }
+        audioQualityComboBox.getSelectionModel().select(2); // SET DEFAULT VALUE TO `GOOD (192 KBPS)`
+
+        // CREATE ARRAY TO STORE BITRATE KEYS AND DISPLAY INSIDE THE BITRATE COMBOBOX
+        bitrateComboBox.getItems().add("Bitrates");
+        sortedList = new ArrayList<>(bitrates.entrySet());
+        sortedList.sort(Map.Entry.comparingByValue());
+        for (Map.Entry<String, Integer> entry : sortedList) {
+            bitrateComboBox.getItems().add(entry.getKey());
+        }
+        bitrateComboBox.getSelectionModel().select(0);
     }
 }
